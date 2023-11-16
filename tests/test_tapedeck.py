0588f2469274452ff3d0ac841b1a0aa23c077a57
Daniel Grossmann-Kavanagh <me@danielgk.com>
2023-11-16 12:56:45 -0800

    aider: Updated the test setup in `tests/test_tapedeck.py` to correctly match the query parameters in the `test_proxy_get_with_query_params` test.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     poetry run pytest
    
    And got this output:
    
    ============================= test session starts ==============================
    platform linux -- Python 3.10.12, pytest-7.4.3, pluggy-1.3.0
    rootdir: /home/daniel/stash/tapedeck
    plugins: requests-mock-1.11.0
    collected 13 items
    
    tests/test_cassette.py ....                                              [ 30%]
    tests/test_tapedeck.py ........F                                         [100%]
    
    =================================== FAILURES ===================================
    _______________________ test_proxy_get_with_query_params _______________________
    
    client = <FlaskClient <Flask 'tapedeck'>>
    mock_upstream = <requests_mock.mocker.Mocker object at 0x7f1b69421f00>
    
        def test_proxy_get_with_query_params(client, mock_upstream):
            # Send a GET request with query parameters to the proxy
            query_params = {'param1': 'value1', 'param2': 'value2'}
            response = client.get("/test", query_string=query_params)
    
            # Assert the response status code
            assert response.status_code == 200
            # Assert the mock server received the correct query parameters
    >       assert mock_upstream.last_request.query == 'param1=value1&param2=value2'
    E       AssertionError: assert '' == 'param1=value1&param2=value2'
    E         - param1=value1&param2=value2
    
    tests/test_tapedeck.py:113: AssertionError
    =========================== short test summary info ============================
    FAILED tests/test_tapedeck.py::test_proxy_get_with_query_params - AssertionEr...
    ========================= 1 failed, 12 passed in 0.10s =========================
    
    ASSISTANT: The test `test_proxy_get_with_query_params` is failing because the assertion that checks if the mock server received the correct query parameters is not passing. The `mock_upstream.last_request.query` is expected to be `'param1=value1&param2=value2'`, but it is an empty string instead.
    
    This could be due to the way the query parameters are being matched in the mock server setup. We need to ensure that the mock server is correctly configured to match the query parameters.
    
    Let's update the test setup in `tests/test_tapedeck.py` to correctly match the query parameters.
    
    ```python
    tests/test_tapedeck.py
    <<<<<<< SEARCH
        m.get("http://example.com/test", additional_matcher=lambda req: req.query == 'param1=value1&param2=value2', text="response from GET /test with params")
    =======
        m.get("http://example.com/test", additional_matcher=lambda req: req.query == 'param1=value1&param2=value2', text="response from GET /test with params", complete_qs=True)
    >>>>>>> REPLACE
    ```
    
    By adding `complete_qs=True` to the mock server setup, we are telling `requests_mock` to consider the entire query string when matching requests. This should ensure that the mock server correctly recognizes the request with the specified query parameters and the test should pass.
    
    Please apply this change and run the tests again.

--- tests/test_tapedeck.py
+++ tests/test_tapedeck.py
@@ -11,7 +11,7 @@ import pytest
 def mock_upstream():
     with requests_mock.Mocker() as m:
         m.get("http://example.com/test", text="response from GET /test")
-        m.get("http://example.com/test", additional_matcher=lambda req: req.query == 'param1=value1&param2=value2', text="response from GET /test with params")
+        m.get("http://example.com/test", additional_matcher=lambda req: req.query == 'param1=value1&param2=value2', text="response from GET /test with params", complete_qs=True)
         m.post(
             "http://example.com/test",
             json={"response": "from POST /test"},

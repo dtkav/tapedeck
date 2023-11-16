e2791ef452212ba5617c98f29c0651720954c755
Daniel Grossmann-Kavanagh <me@danielgk.com>
2023-11-16 13:33:52 -0800

    aider: Refactored tests in `tests/test_cassette.py` to follow idiomatic testing patterns for Click.
    # Aider chat conversation:
    
    USER: also follow idiomatic testing patterns for click by updating `tests/test_cassette.py`
    
    ASSISTANT: To follow idiomatic testing patterns for Click, we will use the `click.testing.CliRunner` class to invoke the CLI commands in the tests. This class provides an isolated environment for running the commands and capturing their output. Here's how you can modify the `tests/test_cassette.py` file:
    
    ```python
    tests/test_cassette.py
    <<<<<<< SEARCH
    from unittest.mock import patch, MagicMock
    from cassette import ProxyCLI
    
    @pytest.fixture
    def mock_cli():
        cli = ProxyCLI()
        cli.stdout = MagicMock()
        return cli
    
    def test_history_command(mock_cli):
        with patch('requests.get') as mock_get:
            mock_get.return_value.ok = True
            mock_get.return_value.json.return_value = [
                {"method": "GET", "path": "/test"}
            ]
            mock_cli.onecmd('history')
            mock_cli.stdout.write.assert_called_with('1: GET /test\n')
    
    def test_replay_command_success(mock_cli):
        with patch('requests.post') as mock_post:
            mock_post.return_value.ok = True
            mock_cli.onecmd('replay 1')
            mock_cli.stdout.write.assert_called_with('Request replayed successfully\n')
    
    def test_replay_command_failure(mock_cli):
        with patch('requests.post') as mock_post:
            mock_post.return_value.ok = False
            mock_post.return_value.json.return_value = {"error": "Invalid request index"}
            mock_cli.onecmd('replay 99')
            mock_cli.stdout.write.assert_called_with('Failed to replay request: Invalid request index\n')
    
    def test_exit_command(mock_cli):
        assert mock_cli.onecmd('exit') == True
    =======
    import click
    from click.testing import CliRunner
    from cassette import cli
    
    @pytest.fixture
    def runner():
        return CliRunner()
    
    def test_history_command(runner):
        with runner.isolated_filesystem():
            with patch('requests.get') as mock_get:
                mock_get.return_value.ok = True
                mock_get.return_value.json.return_value = {
                    'history': [{"method": "GET", "path": "/test"}],
                    'next': None,
                    'previous': None,
                    'limit': 10
                }
                result = runner.invoke(cli, ['history'])
                assert result.exit_code == 0
                assert '1: GET /test\n' in result.output
    
    def test_replay_command_success(runner):
        with runner.isolated_filesystem():
            with patch('requests.post') as mock_post:
                mock_post.return_value.ok = True
                result = runner.invoke(cli, ['replay'])
                assert result.exit_code == 0
                assert 'Request replayed successfully\n' in result.output
    
    def test_replay_command_failure(runner):
        with runner.isolated_filesystem():
            with patch('requests.post') as mock_post:
                mock_post.return_value.ok = False
                mock_post.return_value.json.return_value = {"error": "Invalid request index"}
                result = runner.invoke(cli, ['replay'])
                assert result.exit_code == 0
                assert 'Failed to replay request: Invalid request index\n' in result.output
    >>>>>>> REPLACE
    ```
    
    In this modification, we're using `CliRunner` to invoke the CLI commands and check their output and exit codes. We've replaced the `mock_cli` fixture with a `runner` fixture that provides a `CliRunner` instance. The tests now use `runner.invoke` to call the CLI commands and assert on the `result` object's `exit_code` and `output`.
    
    Please apply these changes to your `tests/test_cassette.py` file to follow idiomatic testing patterns for Click.

--- tests/test_cassette.py
+++ tests/test_cassette.py
@@ -1,36 +1,41 @@
 # Test cases for cassette.py CLI commands
 
 import pytest
-from unittest.mock import patch, MagicMock
-from cassette import ProxyCLI
+import click
+from click.testing import CliRunner
+from cassette import cli
 
 @pytest.fixture
-def mock_cli():
-    cli = ProxyCLI()
-    cli.stdout = MagicMock()
-    return cli
+def runner():
+    return CliRunner()
 
-def test_history_command(mock_cli):
-    with patch('requests.get') as mock_get:
-        mock_get.return_value.ok = True
-        mock_get.return_value.json.return_value = [
-            {"method": "GET", "path": "/test"}
-        ]
-        mock_cli.onecmd('history')
-        mock_cli.stdout.write.assert_called_with('1: GET /test\n')
+def test_history_command(runner):
+    with runner.isolated_filesystem():
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.ok = True
+            mock_get.return_value.json.return_value = {
+                'history': [{"method": "GET", "path": "/test"}],
+                'next': None,
+                'previous': None,
+                'limit': 10
+            }
+            result = runner.invoke(cli, ['history'])
+            assert result.exit_code == 0
+            assert '1: GET /test\n' in result.output
 
-def test_replay_command_success(mock_cli):
-    with patch('requests.post') as mock_post:
-        mock_post.return_value.ok = True
-        mock_cli.onecmd('replay 1')
-        mock_cli.stdout.write.assert_called_with('Request replayed successfully\n')
+def test_replay_command_success(runner):
+    with runner.isolated_filesystem():
+        with patch('requests.post') as mock_post:
+            mock_post.return_value.ok = True
+            result = runner.invoke(cli, ['replay'])
+            assert result.exit_code == 0
+            assert 'Request replayed successfully\n' in result.output
 
-def test_replay_command_failure(mock_cli):
-    with patch('requests.post') as mock_post:
-        mock_post.return_value.ok = False
-        mock_post.return_value.json.return_value = {"error": "Invalid request index"}
-        mock_cli.onecmd('replay 99')
-        mock_cli.stdout.write.assert_called_with('Failed to replay request: Invalid request index\n')
-
-def test_exit_command(mock_cli):
-    assert mock_cli.onecmd('exit') == True
+def test_replay_command_failure(runner):
+    with runner.isolated_filesystem():
+        with patch('requests.post') as mock_post:
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = {"error": "Invalid request index"}
+            result = runner.invoke(cli, ['replay'])
+            assert result.exit_code == 0
+            assert 'Failed to replay request: Invalid request index\n' in result.output
